736ac265802ebd3215d85104ed517bda5e41eeba
이교형 <kevin1371@naver.com>
2026-05-06 01:57:19 +0900

    [feat] 5주차 SwaggerConfig 수정

--- src/main/java/com/example/leets_project/common/config/SwaggerConfig.java
+++ src/main/java/com/example/leets_project/common/config/SwaggerConfig.java
@@ -1,7 +1,10 @@
 package com.example.leets_project.common.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,11 +13,27 @@ import org.springframework.context.annotation.Configuration;
 public class SwaggerConfig {
 
     @Bean
-    public OpenAPI swagger() {
-        Info info = new Info().title("Leets 7th BE Blog API").description("Leets Study API 명세서").version("0.0.1");
+    public OpenAPI LEETSAPI() {
+        Info info = new Info()
+                .title("LEETS API")
+                .description("LEETS API 명세서")
+                .version("1.0.0");
+
+        String jwtSchemeName = "JWT TOKEN";
+        // API 요청헤더에 인증정보 포함
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwtSchemeName);
+        // SecuritySchemes 등록
+        Components components = new Components()
+                .addSecuritySchemes(jwtSchemeName, new SecurityScheme()
+                        .name(jwtSchemeName)
+                        .type(SecurityScheme.Type.HTTP) // HTTP 방식
+                        .scheme("bearer")
+                        .bearerFormat("JWT"));
 
         return new OpenAPI()
+                .addServersItem(new Server().url("/"))
                 .info(info)
-                .addServersItem(new Server().url("/"));
+                .addSecurityItem(securityRequirement)
+                .components(components);
     }
 }
