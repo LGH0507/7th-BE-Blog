9e5057c4a04759c0b63be98a903ec977ec6bb6fe
이교형 <kevin1371@naver.com>
2026-05-05 23:57:46 +0900

    [feat] 각 dto에 @schema 추가

--- src/main/java/com/example/leets_project/domain/comment/web/dto/CommentCreateRequest.java
+++ src/main/java/com/example/leets_project/domain/comment/web/dto/CommentCreateRequest.java
@@ -1,5 +1,6 @@
 package com.example.leets_project.domain.comment.web.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
@@ -13,5 +14,6 @@ public class CommentCreateRequest {
 
     @NotBlank(message = "댓글 내용은 필수입니다.")
     @Size(max =255, message = "댓글은 255자 이하여야 합니다.")
+    @Schema(description = "댓글 내용", example = "댓글입니다.", requiredMode = Schema.RequiredMode.REQUIRED)
     private String content;
 }
